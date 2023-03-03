182c1f0cd1de598ad7a62bab7e2c3c1ffbad728e
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-03 14:46:07 -0500

    Set up React Router with routes for Mail and EmailList

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Sidebar from './components/Sidebar';
+import Mail from './components/Mail';
+import EmailList from './components/EmailList';
+
 
 function App() {
   return (
-    <div className="app">
-      <Header />
-      <Sidebar />
-    </div>
+    <Router>
+      <div className="app">
+        <Header />
+        <div className='app__body'>
+          <Sidebar />
+
+          <Routes>
+            <Route path="/mail" element={<Mail />} />              
+            <Route path="/" element={<EmailList />} />              
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/EmailList.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/EmailList.css';
+
+function EmailList() {
+  return (
+    <div className='emailList'>
+      
+    </div>
+  )
+}
+
+export default EmailList
