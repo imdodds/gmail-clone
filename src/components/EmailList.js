f0d95be598f0de60efdc6394789bdbee82f0e086
Ian Dodds <ianmitchelldodds@gmail.com>
2023-03-03 15:55:09 -0500

    Add Section component to EmailList

--- src/components/EmailList.js
+++ src/components/EmailList.js
@@ -1,10 +1,57 @@
 import React from 'react'
+import { IconButton } from '@mui/material';
+import Checkbox from '@mui/material/Checkbox';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import RedoIcon from '@mui/icons-material/Redo';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import ChevronLeftIcon from '@mui/icons-material/ChevronLeft';
+import ChevronRightIcon from '@mui/icons-material/ChevronRight';
+import KeyboardHideIcon from '@mui/icons-material/KeyboardHide';
+import SettingsIcon from '@mui/icons-material/Settings';
+import InboxIcon from '@mui/icons-material/Inbox';
+import PeopleIcon from '@mui/icons-material/People';
+import LocalOfferIcon from '@mui/icons-material/LocalOffer';
+
 import '../styles/EmailList.css';
+import Section from './Section';
 
 function EmailList() {
   return (
     <div className='emailList'>
-      
+      <div className='emailList__settings'>
+        <div className='emailList__settingsLeft'>
+          <Checkbox />
+          <IconButton>
+            <ArrowDropDownIcon />
+          </IconButton>
+          <IconButton>
+            <RedoIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+        <div className='emailList__settingsRight'>
+          <IconButton>
+            <ChevronLeftIcon />
+          </IconButton>
+          <IconButton>
+            <ChevronRightIcon />
+          </IconButton>
+          <IconButton>
+            <KeyboardHideIcon />
+          </IconButton>
+          <IconButton>
+            <SettingsIcon />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='emailList__sections'>
+        <Section Icon={InboxIcon} title='Primary' color='red' selected />
+        <Section Icon={PeopleIcon} title='Social' color='#1a73e8' />
+        <Section Icon={LocalOfferIcon} title='Promotions' color='green' />
+      </div>
     </div>
   )
 }
